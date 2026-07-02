15d6f6a615f4e8cfbfc81077d17901827b4c1419
vijaya durga bandaru <vijayabandaru73@gmail.com>
2026-07-02 05:20:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.9 MB (76.46%)

---
+++ 0783-search-in-a-binary-search-tree/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root==None:
+            return None
+        if root.val==val:
+            return root
+        elif val<root.val:
+            return self.searchBST(root.left,val) 
+        else:
+            return self.searchBST(root.right,val)
