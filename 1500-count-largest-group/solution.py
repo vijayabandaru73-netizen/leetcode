e0d0f649b147a9086ba04a5a9edbc7d96ec598e0
vijaya durga bandaru <vijayabandaru73@gmail.com>
2026-07-08 06:03:14 +0000

    Sync LeetCode submission Runtime - 39 ms (51.00%), Memory - 19.5 MB (19.10%)

---
+++ 1500-count-largest-group/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        z=Counter(sum(map(int,str(v))) for v in range(1,n+1))
+        zz=Counter(z.values())
+        return max(zz.items())[1]
