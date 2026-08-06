74cd58a8f134806da0ff0041fcc46605c4f478f7
vijaya durga bandaru <vijayabandaru73@gmail.com>
2026-08-06 13:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (69.78%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x: x[0] - x[1])
+        n = len(costs) // 2
+        total_cost = 0
+        for i in range(n):
+            total_cost += costs[i][0] + costs[i + n][1]
+        return total_cost
+        
+        
