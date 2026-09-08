a7015d5861f79042aeaa5d3ae4fc8ca0b0efc7f1
vijaya durga bandaru <vijayabandaru73@gmail.com>
2026-09-08 14:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.20%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {")": "(", "}": "{", "]": "["}
+        
+        for bracket in s:
+            if bracket in ["(", "{", "["]:
+                stack.append(bracket)
+            elif bracket in bracket_map:
+                if not stack or stack.pop() != bracket_map[bracket]:
+                    return False
+        
+        return not stack
+        
